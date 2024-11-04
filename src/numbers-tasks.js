650c748d9d1077c6c91ebc635a1051a8f01d6b94
guinitum <artking01kg@gmail.com>
2024-11-04 18:41:25 +0300

    feat: done getSumOfDigits

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -312,8 +312,17 @@ function getSumToN(n) {
  *   202 => 4  // (2+0+2)
  *   5   => 5  // 5
  */
-function getSumOfDigits(/* num */) {
-  throw new Error('Not implemented');
+function getSumOfDigits(num) {
+  let sum = num;
+  while (sum > 9) {
+    let tempSum = 0;
+    while (sum > 0) {
+      tempSum += sum % 10;
+      sum = Math.floor(sum / 10);
+    }
+    sum = tempSum;
+  }
+  return sum;
 }
 
 /**
