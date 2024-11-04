010bf988ee4eeba2aedb1d8e7c3272272fc835de
guinitum <artking01kg@gmail.com>
2024-11-04 18:28:37 +0300

    feat: done getSumToN

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -298,10 +298,9 @@ function getFibonacciNumber(index) {
  *   10 => 55 // (1+2+3+...+10)
  *   1  => 1
  */
-function getSumToN(/* n */) {
-  throw new Error('Not implemented');
+function getSumToN(n) {
+  return (n * (n + 1)) / 2;
 }
-
 /**
  * Returns the sum of the digits of a given number.
  *
